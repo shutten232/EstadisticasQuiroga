c37c054fba0e6f003b6be7e16088a3569e8d239f
gustavo castanares <115637674+shutten232@users.noreply.github.com>
2025-12-12 16:24:56 -0300

    actualizacion

--- data/static-data.js
+++ data/static-data.js
@@ -1,5 +1,17 @@
-export const labelsMeses = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct'];
-export const dataCRPC  = [947,655,557,692,660,777,1027,931,955,1099];
-export const dataObleasTotales = [2586,1571,1926,2179,2049,2096,2600,2535,2505,2545];
-export const impactoCRPC   = [16,14,13,15,16,16,15,16,16,18];
-export const impactoObleas = [13,10,11,11,11,11,12,12,12,12];
+// data/static-data.js
+
+export const labelsMeses = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov'];
+
+// CRPC por mes (Nov = 891)
+export const dataCRPC = [947,655,557,692,660,777,1027,931,955,1099,891];
+
+// Obleas totales del taller por mes (Quiroga + Quirgroup)
+// Nov = 2.169 (1.794 + 375)
+export const dataObleasTotales = [2586,1571,1926,2179,2049,2096,2600,2535,2505,2545,2169];
+
+// Impacto mensual (%)
+// Nov CRPC = 18,7% (891 / 4.769)
+export const impactoCRPC = [16,14,13,15,16,16,15,16,16,18,18.7];
+
+// Nov PEC = 12,0% (2.169 / 18.096)
+export const impactoObleas = [13,10,11,11,11,11,12,12,12,12,12.0];
